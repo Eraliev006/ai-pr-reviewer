09e552c535ae13cc47952f8744148907c94897f2
Eraliev006 <eraliev.adilet006@gmail.com>
2026-01-28 03:09:34 +0600

    refactor: - add greenlet - add import config from core

--- alembic/env.py
+++ alembic/env.py
@@ -7,6 +7,7 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from app.core.db import BaseModel
+from app.core import config as config_
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -28,7 +29,7 @@ target_metadata = BaseModel.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-config.set_main_option("sqlalchemy.url", config.DATABASE_URL)
+config.set_main_option("sqlalchemy.url", config_.DATABASE_URL)
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
