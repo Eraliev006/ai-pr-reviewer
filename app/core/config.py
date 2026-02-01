4cae37840e4e53dd6a62b2d98e849e80dea54f82
Eraliev006 <eraliev.adilet006@gmail.com>
2026-02-01 19:47:12 +0600

    replace psycopg to asyncpg

--- app/core/config.py
+++ app/core/config.py
@@ -29,6 +29,6 @@ class Config(BaseSettings):
 
     @property
     def DATABASE_URL(self) -> str:
-        return f"postgresql+psycopg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        return f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
 
 config = Config()
